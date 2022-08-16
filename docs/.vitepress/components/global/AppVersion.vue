1e1893c8b92672841d07224493b9520b47f7e9c8
Anton Reshetov <reshetov.art@gmail.com>
2022-08-16 15:09:59 +0300

    feat(ui): add AppVersion tag

---
+++ docs/.vitepress/components/global/AppVersion.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app-version">
+    {{ text }}
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  text: string
+}
+
+defineProps<Props>()
+</script>
+
+<style lang="scss">
+.app-version {
+  display: inline-flex;
+  padding: 0 6px;
+  background: var(--vp-c-yellow-light);
+  font-size: 12px;
+  font-family: var(--font-code);
+  border-radius: 3px;
+  @media (prefers-color-scheme: dark) {
+     background: var(--vp-c-yellow-dark);
+  }
+}
+
+.dark {
+  .app-version {
+     background: var(--vp-c-yellow-darker);
+  }
+}
+</style>
