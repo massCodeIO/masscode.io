eedb67c71a9dd7c4634bf69ad5853cf58d728839
Anton Reshetov <reshetov.art@gmail.com>
2022-08-17 17:44:56 +0300

    feat(scripts): add generate assets

--- docs/download/index.md
+++ docs/download/index.md
@@ -6,26 +6,27 @@ title: Download
 
 ## macOS
 
-<VPButton theme="brand" text="Download for Apple Silicon" />
+<VPButton :href="assets.macM1" theme="brand" text="Download for Apple Silicon" />
 
 macOS 11.0+
 
-<VPButton theme="brand" text="Download for Intel" />
+<VPButton :href="assets.mac" theme="brand" text="Download for Intel" />
 
 macOS 10.13+
 
 ## Windows
 
-<VPButton theme="brand" text="Download for Windows" />
+<VPButton :href="assets.win" theme="brand" text="Download for Windows" />
 
 Windows 10, 11
 
 ## Linux
 
-<VPButton theme="brand" text="Download for Linux" />
+<VPButton :href="assets.linux" theme="brand" text="Download for Linux" />
 
 Ubuntu
 
 <script setup lang="ts">
 import VPButton from 'vitepress/client/theme-default/components/VPButton.vue'
+import assets from '../.vitepress/_data/assets.json'
 </script>
---
+++ docs/scripts/generate-assets.ts
@@ -0,0 +1,41 @@
+import { join } from 'path'
+import { writeJSONSync } from 'fs-extra'
+import * as dotenv from 'dotenv'
+import axios from 'axios'
+
+dotenv.config({ path: join(__dirname, '../.env') })
+
+const BASE_URL = 'https://api.github.com'
+const RELEASES_URL = '/repos/massCodeIO/massCode/releases'
+const DIST = join(__dirname, '../.vitepress/_data')
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `token ${process.env.VITE_GIT_TOKEN}`
+  }
+})
+
+const fetch = async () => {
+  const { data } = await api.get(RELEASES_URL)
+
+  const releases = data.filter(i => !i.prerelease)
+  const tagName = releases[0].tag_name
+  const version = tagName.substring(1)
+  const macAsset = `massCode-${version}.dmg`
+  const macM1Asset = `massCode-${version}-arm64.dmg`
+  const winAsset = `massCode.Setup.${version}.exe`
+  const linuxAsset = `massCode_${version}_amd64.snap`
+  const downloadUrl = `https://github.com/massCodeIO/massCode/releases/download/${tagName}`
+
+  const assets = {
+    mac: `${downloadUrl}/${macAsset}`,
+    macM1: `${downloadUrl}/${macM1Asset}`,
+    win: `${downloadUrl}/${winAsset}`,
+    linux: `${downloadUrl}/${linuxAsset}`
+  }
+
+  writeJSONSync(join(DIST, 'assets.json'), assets)
+}
+
+fetch()
