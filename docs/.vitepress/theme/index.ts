653c2879108c4b9ee1f9124064927098dfd65eca
Anton Reshetov <reshetov.art@gmail.com>
2022-08-17 17:38:53 +0300

    chore: add GA

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,14 @@
 import DefaultTheme from 'vitepress/theme'
 import type { EnhanceAppContext } from 'vitepress'
+import VueGtag, { pageview } from 'vue-gtag'
+import { watch } from 'vue'
 import AppLink from '../components/global/AppLink.vue'
 import AppVersion from '../components/global/AppVersion.vue'
 import CodeEditor from '../components/global/CodeEditor.vue'
 import CodePreview from '../components/global/CodePreview.vue'
 import CollectionPreview from '../components/global/CollectionPreview.vue'
-
 import './styles.scss'
+
 export default {
   ...DefaultTheme,
   enhanceApp (context: EnhanceAppContext) {
@@ -15,5 +17,14 @@ export default {
     context.app.component('CodeEditor', CodeEditor)
     context.app.component('CodePreview', CodePreview)
     context.app.component('CollectionPreview', CollectionPreview)
+
+    context.app.use(VueGtag, {
+      config: { id: import.meta.env.VITE_GA }
+    })
+
+    watch(() => context.router.route.data.relativePath, () => {
+      // @ts-expect-error автотрекинг
+      pageview(context.router.route.path)
+    })
   }
 }
