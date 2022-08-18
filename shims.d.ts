1bff436155617f7d59917bc0f49666af8ce65bab
Anton Reshetov <reshetov.art@gmail.com>
2022-08-18 08:48:53 +0300

    types: fix provide shims

--- shims.d.ts
+++ shims.d.ts
@@ -1,6 +1,12 @@
+/// <reference types="vitepress/client" />
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+interface ImportMetaEnv {
+  VITE_GA: string
+  VITE_GIT_TOKEN: string
 }
