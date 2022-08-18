36734d770caf69de39a46225ab5fb5d881dc4129
Anton Reshetov <reshetov.art@gmail.com>
2022-08-18 06:31:50 +0300

    feat: add sponsors

---
+++ docs/.vitepress/components/sponsors/SponsorsGroup.vue
@@ -0,0 +1,107 @@
+<template>
+  <div
+    ref="sponsorsRef"
+    class="sponsors"
+  >
+    <template v-if="isShow">
+      <h2>{{ title }} sponsors</h2>
+      <div
+
+        class="body"
+      >
+        <a
+          v-for="{ img, url, name } of data[tier]"
+          :key="name"
+          :href="url"
+          target="_blank"
+          class="sponsors-item"
+          rel="noopener sponsored"
+        >
+          <img
+            :src="img"
+            :alt="name"
+          >
+        </a>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue'
+import sponsorsData from './sponsors.json'
+
+interface Sponsor {
+  url: string
+  img: string
+  name: string
+  description?: string
+}
+
+interface SponsorData {
+  special: Sponsor[]
+}
+
+interface Props {
+  title: string
+  tier: keyof SponsorData
+}
+
+defineProps<Props>()
+
+const data = ref<SponsorData>(sponsorsData)
+const isShow = ref(false)
+
+const sponsorsRef = ref<HTMLElement>()
+
+const showSponsors = () => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        isShow.value = true
+        observer.disconnect()
+      }
+    },
+    { rootMargin: '0px 0px 300px 0px' }
+  )
+  observer.observe(sponsorsRef.value)
+
+  onUnmounted(() => observer.disconnect())
+}
+
+onMounted(async () => {
+  showSponsors()
+})
+</script>
+
+<style lang="scss" scoped>
+.sponsors {
+  margin-top: 48px;
+  .body {
+    margin-top: 24px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 24px;;
+  }
+  h2 {
+    font-size: 20px;
+    font-weight: 600;
+  }
+}
+.sponsors-item {
+  background-color: var(--vp-c-bg-soft);
+  padding: 24px;
+  border-radius: 4px;
+}
+
+.dark {
+  .sponsors-item {
+    img {
+      filter: grayscale(1) invert(1);
+      &:hover {
+        filter:  none;
+      }
+    }
+  }
+}
+</style>
---
+++ docs/.vitepress/components/sponsors/SponsorsItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sponsors-item">
+    <img
+      :src="src"
+      alt=""
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+type Brand = 'digitalocean'
+
+interface Props {
+  brand: Brand
+}
+
+const props = defineProps<Props>()
+
+const src = 'https://opensource.nyc3.cdn.digitaloceanspaces.com/attribution/assets/PoweredByDO/DO_Powered_by_Badge_blue.svg'
+</script>
+
+<style lang="scss" scoped>
+.sponsors-item {
+  background-color: var(--vp-c-bg-soft);
+  padding: 24px;
+  border-radius: 4px;
+}
+
+.dark {
+  .sponsors-item {
+    img {
+      filter: grayscale(1) invert(1);
+      &:hover {
+        filter:  none;
+      }
+    }
+  }
+}
+</style>
