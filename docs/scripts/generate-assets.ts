e9516cff8a862fefd6cd202480458ae9d579700c
Anton Reshetov <reshetov.art@gmail.com>
2022-08-29 12:30:04 +0300

    feat(scripts): generate latest release page

--- docs/scripts/generate-assets.ts
+++ docs/scripts/generate-assets.ts
@@ -1,13 +1,15 @@
 import { join } from 'path'
-import { writeJSONSync } from 'fs-extra'
+import { writeFileSync, writeJSONSync } from 'fs-extra'
 import * as dotenv from 'dotenv'
 import axios from 'axios'
+import handlebars from 'handlebars'
 
 dotenv.config({ path: join(__dirname, '../.env') })
 
 const BASE_URL = 'https://api.github.com'
 const RELEASES_URL = '/repos/massCodeIO/massCode/releases'
 const DIST = join(__dirname, '../.vitepress/_data')
+const DOWNLOAD_PATH = join(__dirname, '../download/')
 
 const api = axios.create({
   baseURL: BASE_URL,
@@ -15,6 +17,15 @@ const api = axios.create({
     Authorization: `token ${process.env.VITE_GIT_TOKEN}`
   }
 })
+
+const templateLatestRelease = handlebars.compile(`---
+title: Latest Release
+---
+# v{{ version }}
+
+{{ content }}
+
+<AssetsDownload />`)
 
 const fetch = async () => {
   const { data } = await api.get(RELEASES_URL)
@@ -27,16 +38,21 @@ const fetch = async () => {
   const winAsset = `massCode.Setup.${version}.exe`
   const linuxAsset = `massCode_${version}_amd64.snap`
   const downloadUrl = `https://github.com/massCodeIO/massCode/releases/download/${tagName}`
+  const changelog = releases[0].body
 
   const assets = {
     version,
+    changelog,
     mac: `${downloadUrl}/${macAsset}`,
     macM1: `${downloadUrl}/${macM1Asset}`,
     win: `${downloadUrl}/${winAsset}`,
     linux: `${downloadUrl}/${linuxAsset}`
   }
 
+  const latestReleaseMd = templateLatestRelease({ version, content: changelog })
+
   writeJSONSync(join(DIST, 'assets.json'), assets)
+  writeFileSync(join(DOWNLOAD_PATH, 'latest-release.md'), latestReleaseMd, 'utf-8')
 }
 
 fetch()
