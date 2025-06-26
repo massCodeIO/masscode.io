f11f35c4dc4772eae8ac7d055cd0ade5fc56d9aa
Anton Reshetov <reshetov.art@gmail.com>
2025-06-26 16:25:47 +0300

    build: fix gtag initialization for SSR

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,6 @@ import type { EnhanceAppContext } from 'vitepress'
 import VPButton from 'vitepress/dist/client/theme-default/components/VPButton.vue'
 import DefaultTheme from 'vitepress/theme'
 import { h, watch } from 'vue'
-import { configure, pageview } from 'vue-gtag'
 import AppLink from '../components/global/AppLink.vue'
 import AppVersion from '../components/global/AppVersion.vue'
 import AssetsDownload from '../components/global/AssetsDownload.vue'
@@ -14,12 +13,20 @@ import SidebarSponsors from '../components/sponsors/SidebarSponsors.vue'
 import './styles.scss'
 
 function initGtag(context: EnhanceAppContext) {
-  configure({
-    tagId: import.meta.env.VITE_GA,
-  })
+  if (import.meta.env.SSR)
+    return
 
-  watch(() => context.router.route.data.relativePath, () => {
-    pageview(context.router.route.path)
+  import('vue-gtag').then(({ configure, pageview }) => {
+    configure({
+      tagId: import.meta.env.VITE_GA,
+    })
+
+    watch(
+      () => context.router.route.data.relativePath,
+      () => {
+        pageview(context.router.route.path)
+      },
+    )
   })
 }
 
